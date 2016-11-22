a0e0adcee9393be815e47e1defa68f979c3d25e2
mvLaboratory <mvlabaratory@gmail.com>
2016-11-22 14:21:24 +0200

    10.10 init

--- src/com/javarush/test/level18/lesson10/home10/Solution.java
+++ src/com/javarush/test/level18/lesson10/home10/Solution.java
@@ -11,73 +11,8 @@ package com.javarush.test.level18.lesson10.home10;
 Закрыть потоки. Не использовать try-with-resources
 */
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-
 public class Solution {
     public static void main(String[] args) {
-        HashMap<String, String> fileMap = new  HashMap<String, String>();
-        ArrayList<String> fileParts = new ArrayList<String>();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String filename = "";
-        while(!filename.equalsIgnoreCase("end")) {
-            try
-            {
-                filename = reader.readLine();
-                if (filename.equalsIgnoreCase("end")) {
-                    break;
-                }
-                int dotIndex = filename.lastIndexOf(".");
-                fileParts.add(filename.substring(dotIndex + 1));
-                fileMap.put(filename.substring(dotIndex + 1), filename);
-            }
-            catch (IOException e) {
-
-            }
-        }
-
-        try {
-            reader.close();
-        }
-        catch (IOException e) {
-
-        }
-
-        Collections.sort(fileParts);
-        if (fileParts.size() == 0 || fileMap.size() == 0) {
-            return;
-        }
-        String outputFileFullName = fileMap.get(fileParts.get(0));
-        String outputFileName = outputFileFullName.substring(0, outputFileFullName.lastIndexOf("."));
-
-        try
-        {
-            FileOutputStream fileOut = new FileOutputStream(outputFileName);
-
-            for (String filePart : fileParts) {
-                try {
-                    FileInputStream file = new FileInputStream(fileMap.get(filePart));
-
-                    int available = file.available();
-                    byte[] buffer = new byte[available];
-                    if (available > 0) {
-                        file.read(buffer);
-                        fileOut.write(buffer);
-                    }
-
-                    file.close();
-                }
-                catch (IOException e)
-                {
-                }
-            }
-            fileOut.close();
-        }
-        catch (IOException e) {
-
-        }
     }
 }
