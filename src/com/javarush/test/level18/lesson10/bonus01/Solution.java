32eab2bada52b832923c750c6aabdfb0242fdca5
mvLaboratory <mvLabaratory@gmail.com>
2016-12-08 23:27:50 +0200

    18.10.b2

--- src/com/javarush/test/level18/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level18/lesson10/bonus01/Solution.java
@@ -13,9 +13,48 @@ fileOutputName - имя файла, куда необходимо записат
 -d - ключ указывает, что необходимо расшифровать данные
 */
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length < 3)
+        {
+            return;
+        }
 
+        String cryption = args[0];
+        String fileName = args[1];
+        String fileOutputName = args[2];
+
+        int direction = 0;
+        if (cryption.equals("-e")) {
+            direction = 1;
+        }
+        else if (cryption.equals("-d")) {
+            direction = -1;
+        }
+        else {
+            return;
+        }
+
+        FileInputStream in = new FileInputStream(new File(fileName));
+        byte[] buffer = new byte[in.available()];
+        while (in.available() > 0)
+        {
+            in.read(buffer);
+        }
+        in.close();
+
+        FileOutputStream resultFile = new FileOutputStream(fileOutputName);
+        for (int i = 0; i < buffer.length; i++)
+        {
+            buffer[i] = (byte) (buffer[i] + (10 * direction));
+        }
+        resultFile.write(buffer);
+        resultFile.close();
     }
-
 }
